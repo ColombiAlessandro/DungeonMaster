bdec42872e3db39b9ee91ed0516f4656c4db1d10
ColombiAlessandro <colombi.alessandro.studente@itispaleocapa.it>
2022-03-23 13:48:12 +0100

    Miglioramento animazione
    inizio sistema movimento per aggiustarlo in base alla direzione

--- DungeonMaster/Assets/Script/Player.cs
+++ DungeonMaster/Assets/Script/Player.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //velocità giocatore 
     [SerializeField] float velocita;
     private BoxCollider2D pg;
+    private Animator animazione;
     void Start()
     {
         pg = GetComponent<BoxCollider2D>();
+        animazione = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
@@ -17,6 +20,11 @@ public class Player : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        //movimento giocatore
         transform.Translate(Input.GetAxis("Horizontal") * velocita, Input.GetAxis("Vertical") * velocita, 0);
+
+        animazione.SetBool("Sinistra", Input.GetAxis("Horizontal") < 0);
+        animazione.SetBool("Sopra", Input.GetAxis("Vertical") > 0);
+
     }
 }
