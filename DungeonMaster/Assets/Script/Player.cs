439b261cb6cd84e2cb61a5ec73dacfe5acc04bff
ColombiAlessandro <colombi.alessandro.studente@itispaleocapa.it>
2022-04-11 17:46:56 +0200

    Animazione attacco
    aggiunta ed implementazione di animazioni durante la fase di attacco

--- DungeonMaster/Assets/Script/Player.cs
+++ DungeonMaster/Assets/Script/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,6 +8,8 @@ public class Player : MonoBehaviour
     //dichiarazioni
     [SerializeField] float velocita;
     [SerializeField] int vite;
+    public static Timer timerAttacco;
+    public bool attacco;
     public Barra barra;
     private BoxCollider2D pg;
     private Rigidbody2D rb;
@@ -19,11 +22,24 @@ public class Player : MonoBehaviour
         pg = GetComponent<BoxCollider2D>();
         animazione = GetComponent<Animator>();
         barra.VitaMassima(vite);
+        timerAttacco = new Timer(1100);
+        timerAttacco.Elapsed += TimerAttacco_Elapsed;
     }
+
+    private void TimerAttacco_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        attacco = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            attacco = true;
+            timerAttacco.Start();
+            Animazione();
+        }
     }
     private void FixedUpdate()
     {
@@ -31,13 +47,20 @@ public class Player : MonoBehaviour
         Movimento();
         //animazione movimento
         Animazione();
-
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Nemico"))
         {
-            Danno();
+            if (attacco)
+            {
+                Debug.Log("Colpito");
+            }
+            else
+            {
+                Danno();
+            }
         }
     }
     private void Movimento()
@@ -51,6 +74,7 @@ public class Player : MonoBehaviour
         animazione.SetFloat("Orizzontale", movimento.x);
         animazione.SetFloat("Verticale", movimento.y);
         animazione.SetFloat("Velocità", movimento.sqrMagnitude);
+        animazione.SetBool("Attacco", attacco);
     }
     private void Danno()
     {
