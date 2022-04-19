4405360a42de66fcdc3a92b1e462e9a197a34888
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2022-04-19 03:57:55 +0200

    problema giocatore risolto+animazione morte e danno(knockback da finire)
    In questa versione ho risolto il problema del giocatore che non cambiava direzione quando non veniva premuto nulla. Inoltre ho fatto le animazioni della morte e del ricevimento danni del player.Devo ancora finire il Knockback.

--- DungeonMaster/Assets/Script/Player.cs
+++ DungeonMaster/Assets/Script/Player.cs
@@ -9,7 +9,8 @@ public class Player : MonoBehaviour
     //dichiarazioni
     [SerializeField] float velocita;
     [SerializeField] int vite;
-    public bool attacco;
+    private int i = 0;
+    public bool attacco, attaccaFermo,colpito, fermo = true;
     public Barra barra;
     private BoxCollider2D pg;
     public Rigidbody2D rb;
@@ -17,7 +18,8 @@ public class Player : MonoBehaviour
     public Vector2 movimento;
     private float cooldown = 0;
     private float durata;
-    private float invulnerabilità=0;
+    private float invulnerabilità = 0;
+    public Transform pos;
     void Start()
     {
         //assegnazione variabili
@@ -25,23 +27,28 @@ public class Player : MonoBehaviour
         pg = GetComponent<BoxCollider2D>();
         animazione = GetComponent<Animator>();
         barra.VitaMassima(vite);
+        pos = gameObject.GetComponent<Transform>();
     }
     // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetMouseButtonDown(0))
         {
             if (Time.time >= cooldown)
             {
                 //cdAttacco.Reset();
                 attacco = true;
+                fermo = true;
+                UnityEngine.Debug.Log(fermo);
+                UnityEngine.Debug.Log(attacco);
                 gameObject.tag.Replace("Player", "Attacco");
                 animazione.SetBool("Attacco", true);
                 //timerAttacco.Start();
                 cooldown = Time.time + 1.0f;
                 durata = Time.time + 0.5f;
             }
+
         }
         if (Time.time >= durata)
         {
@@ -49,18 +56,42 @@ public class Player : MonoBehaviour
             animazione.SetBool("Attacco", false);
             gameObject.tag.Replace("Attacco", "Player");
         }
+
+        if (colpito == true)
+        {
+            //StartCoroutine(Knockback());
+            colpito = false;
+        }
     }
     private void FixedUpdate()
     {
-        //movimento giocatore
-        Movimento();
-        //animazione movimento
-        Animazione();
+        if (Input.GetKey(KeyCode.W) == true || Input.GetKey(KeyCode.A) == true || Input.GetKey(KeyCode.S) == true || Input.GetKey(KeyCode.D) == true)
+        {
+            animazione.SetBool("fermo", false);
+            //movimento giocatore
+            Movimento();
+            //animazione movimento
+            Animazione();
+        }
+        else
+        {
+            animazione.SetBool("fermo", true);
+            if (attacco == true && fermo == true)
+            {
+                attaccaFermo = true;
+                animazione.SetBool("attaccaFermo", true);
+            }
+            if (attacco == false && fermo == true && attaccaFermo == true)
+            {
+                animazione.SetBool("attaccaFermo", false);
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Nemico"))
         {
+            animazione.SetBool("Colpito", true);
             if (attacco)
             {
                 UnityEngine.Debug.Log("Colpito");
@@ -86,15 +117,62 @@ public class Player : MonoBehaviour
         animazione.SetFloat("Orizzontale", movimento.x);
         animazione.SetFloat("Verticale", movimento.y);
         animazione.SetFloat("Velocità", movimento.sqrMagnitude);
-        
     }
     private void Danno()
     {
         vite--;
         barra.ImpostaVita(vite);
+        animazione.SetBool("Colpito", true);
+        colpito = true;
+
         if (vite == 0)
         {
-            Destroy(gameObject);
+            animazione.SetBool("Morte", true);
+            velocita = 0;
+            //Destroy(gameObject);
         }
+        StartCoroutine(Timer());
     }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(3);
+        animazione.SetBool("Colpito", false);
+    }
+    /*IEnumerator Knockback()
+    {
+
+        if(movimento.x<0 && (movimento.y >= 0 || movimento.y <= 0))
+        {
+            while (i < 10)
+            {
+                gameObject.transform.position = new Vector2(movimento.x - 0.50f,0);
+                yield return null;
+            }
+        }
+        else if (movimento.x > 0 && (movimento.y >= 0 || movimento.y<=0))
+        {
+            while (i < 10)
+            {
+                gameObject.transform.position = new Vector2(movimento.x + 0.50f, 0);
+                yield return null;
+            }
+        }
+        else if ((movimento.x <= 0 || movimento.x >= 0) && movimento.y > 0)
+        {
+            while (i < 10)
+            {
+                gameObject.transform.position = new Vector2(0, movimento.y + 0.50f);
+                yield return null;
+            }
+        }
+        else if ((movimento.x <= 0 || movimento.x >= 0) && movimento.y < 0)
+        {
+            while (i < 10)
+            {
+                
+                gameObject.transform.position = new Vector2(0, movimento.y - 0.50f);
+                yield return null;
+            }
+        }
+    }*/
 }
